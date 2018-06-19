a91d4af3fab03d8483298f2448f7f4b495d4e621
JohnMV13 <thejohnmiesterV@gmail.com>
2018-06-19 13:39:34 -0500

    added the chart to my JavaScript page. Need to continue working on getting the pictures on my page

--- js/app.js
+++ js/app.js
@@ -33,5 +33,76 @@ function Placeholder(name, src) {
 }
 Placeholder.all = [];
 
-new Placeholder("bag", "../img/bag.jpg");
-new Placeholder("breakfast", "img/breakfast.jpg")
+new Placeholder("placekitten.com", "https://placekitten.com/g/150/150", 7, 2);
+new Placeholder("fillmurray.com", "http://fillmurray.com/150/150", 10,10);
+new Placeholder("placecage.com", "http://placecage.com/150/150", 6, 3);
+
+for (var i = 0; i < Placeholder.all.length; i++) {
+  Placeholder.all[i].voteCount = Math.floor(5 + Math.random() * 500);
+  Placeholder.all[i].showCount = Math.floor(20 + Math.random() * 1000);
+}
+
+console.log("all Placeholders", Placeholder.all);
+
+window.addEventListener("load", displayImages);
+
+function showResults() {
+  var ul = document.getElementById("results");
+  ul.innerHTML = "";
+
+  for (var i = 0; i < Placeholder.all.length; i++) {
+    var current = Placeholder.all[i];
+
+    var li = document.createElement("li");
+    li.textContent = current.name + " got " + current.voteCount + " votes";
+    ul.appendChild(li);
+  }
+
+  showResultChart();
+}
+
+function showResultChart() {
+  var canvas = document.getElementById("resultsCanvas");
+
+  canvas.style.display = "block";
+
+  var labels = [];
+  var voteCounts = [];
+  var showCounts = [];
+  var votePercentage = [];
+  for (var i = 0; i < Placeholder.all.length; i++) {
+    labels[i] = Placeholder.all[i].name;
+    voteCounts[i] = Placeholder.all[i].voteCount;
+    showCounts[i] = Placeholder.all[i].showCount;
+    votePercentage[i] = 100 * voteCounts[i] / showCounts[i];
+  }
+
+  var ctx = canvas.getContext("2d");
+  var chart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "Vote Count",
+          backgroundColor: "rgb(121, 0, 158)",
+          data: voteCounts
+        },
+        {
+          label: "Show Count",
+          backgroundColor: "rgb(25, 96, 26)",
+          data: showCounts
+        },
+        {
+          label: "Vote %",
+          backgroundColor: "rgb(0, 0, 0)",
+          data: votePercentage
+        },
+      ]
+    },
+    title: {
+      display: true,
+      text: "Voting Results"
+    }
+  })
+}
